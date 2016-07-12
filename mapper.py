1198f15230c17db856a858c5b493ab5458507fe0
marikina95 <marika.dellabianca@gmail.com>
2016-07-12 16:26:45 +0200

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,12 @@
+def openFile():
+    with open('pulisci-sample-data.txt', 'r') as f:
+        return f.readlines()
+
+def getData(x):
+    for line in x:
+        i = line.split(';')
+        print i[0].strip() + "\t" + i[1].strip() + "\t" + i[2].strip() + "\t" + i[3].strip()
+
+if __name__ == "__main__":
+    x = openFile()
+    getData(x)
